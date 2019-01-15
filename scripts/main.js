f0d0cc149e69ca6b917d13edc8bce691ad79854e
PanchenkoNata <42978056+PanchenkoNata@users.noreply.github.com>
2019-01-15 23:35:11 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,5 @@ let elDiv = document.querySelector('.wrapper');
 let elImg = elDiv.querySelector('img');
 
 setTimeout(function(){
-    elImg.setAttribute('src', 'images/VanyaKiller.png');
+    elImg.setAttribute('src', '/images/VanyaKiller.png');
 }, 1000);
